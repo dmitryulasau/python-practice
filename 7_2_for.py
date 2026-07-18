fc2d85c81f7dc202b82130418802b2d3a52ee595
Dmitry Ulasau <dmitrushok@gmail.com>
2026-07-18 23:29:03 +0300

    add for

---
+++ 7_2_for.py
@@ -0,0 +1,35 @@
+# for meters in 100, 90, 95, 87, 102:
+#  if meters % 3 == 0:
+#    print(meters, 'Подходит')
+#  else:
+#    print(meters, 'Не подходит')
+
+# -------------------------------------
+
+# for exponent in 3, 7, 5, 6, 4:
+#     print(exponent ** 2, exponent ** 3, exponent ** 4)
+
+# -------------------------------------
+
+# winners = 0
+# for ticket in 345, 19, 87, 1020, 421, 10000, 1, 0:
+#     if ticket % 5 == 0 and 100 <= ticket <= 999:
+#         winners += 1
+#         print(f"Билет {ticket} - выиграл!")
+# print(f"Всего победителей: {winners}")
+
+# -------------------------------------
+
+# for number in range(11):
+#     print(number ** 2)
+
+# -------------------------------------
+
+# time = int(input("Который час? "))
+# for qq in range(time):
+#     print("Ку-ку!")
+
+# -------------------------------------
+
+for number in range(21):
+    print(f"2 ** {number} = {2 ** number}")
