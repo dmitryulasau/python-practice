2ca924f0d7daa9242357cc1f92ca5996a82bdb45
Dmitry Ulasau <dmitrushok@gmail.com>
2026-07-22 22:14:35 +0300

    add algo new

--- 8_algo.py
+++ 8_algo.py
@@ -6,7 +6,7 @@
 
 # hours = int(input("Количество часов: "))
 # cells = 1
-# for i in range(1, hours // 3 +1):
+# for i in range(1, hours // 3 + 1):
 #     hours_elapsed = i * 3
 #     cells *= 2
 #     hours_left = hours - hours_elapsed
@@ -37,14 +37,40 @@
 # print(f"Сумма: {result}")
 
 # -------------------------------------------
-wake_up = int(input("Во сколько проснулся Саша? "))
-water = 0
-calories = 0
-for i in range(wake_up, 23, 3):
-    print(f"{i}-й час")
-    water += 1
-    cal = int(input("Сколько съедено калорий: "))
-    calories += cal
-print("Перед сном итог")
-print(f"Всего калорий: {calories}")
-print(f"Всего литров воды: {water}")
+# wake_up = int(input("Во сколько проснулся Саша? "))
+# water = 0
+# calories = 0
+# for i in range(wake_up, 23, 3):
+#     print(f"{i}-й час")
+#     water += 1
+#     cal = int(input("Сколько съедено калорий: "))
+#     calories += cal
+# print()
+# print("Перед сном итог")
+# print(f"Всего калорий: {calories}")
+# print(f"Всего литров воды: {water}")
+
+# -------------------------------------------
+
+# seconds = int(input("Введите кол-во секунд: "))
+# for second in range(seconds, 0, -1):
+#     print(second)
+# print("Я иду искать!")
+
+# -------------------------------------------
+
+# while True:
+#     year = int(input("Введите год (не меньше 1900): "))
+#     if year < 1900:
+#         print("Год должен быть не меньше 1900")
+#     else:
+#         for i in range(year, 1900 - 1, -2):
+#             print(i)
+#         break
+
+# -------------------------------------------
+
+seconds = int(input("Сколько секунд считать: "))
+for i in range(seconds - (seconds % 2), 1, -2):   
+    print(i)
+print("Я иду искать!")
