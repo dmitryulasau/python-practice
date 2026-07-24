52b96d0471978f6c4cf742d324b9b92e9ab54280
Dmitry Ulasau <dmitrushok@gmail.com>
2026-07-24 23:05:47 +0300

    add strings print end

--- 9_strings.py
+++ 9_strings.py
@@ -18,9 +18,59 @@
 
 # -----------------------------------------------------
 
-name = input("Как тебя зовут? ")
-print(f"{name}, купи слона!")
-while True:
-    answer = input()
-    print(f"Все говорят {answer}, а ты купи слона!")
+# name = input("Как тебя зовут? ")
+# print(f"{name}, купи слона!")
+# while True:
+#     answer = input()
+#     print(f"Все говорят {answer}, а ты купи слона!")
+    
+# for symbol in "Python!":
+#     print(symbol)
+
+# -----------------------------------------------------
+
+# text = input("Введите текст: ")
+# for symbol in text:
+#     print(symbol * 3)
+
+# -----------------------------------------------------
+
+# text = input("Введите текст: ")
+# big_letters = 0
+# small_letters = 0
+# for symbol in text:
+#     if symbol == "Ы":
+#         big_letters +=1
+#     elif symbol == "ы":
+#         small_letters += 1
+
+# print(f"Больших букв Ы: {big_letters}")
+# print(f"Маленьких букв Ы: {small_letters}")
+
+# -----------------------------------------------------
+
+# for i in range(6):
+#     if i == 0 or i == 5:
+#         print("-" * 10)
+#     else:
+#         print("|" + ("0" * 8) + "|")
+
+# -----------------------------------------------------
+
+# number = int(input("Введите число: "))
+# step = int(input("Введите шаг: "))
+# number_sum = 0
+# for i in range(3):
+#     print(number, end=".")
+#     number_sum += number
+#     number += step
+# print(number_sum)
+
+# -----------------------------------------------------
+
+number = int(input("Введите число: ")) 
+print("-=-", end="") 
+for i in range(0, number + 1, 10): 
+    print(i, end="-=-")
+print()
     
