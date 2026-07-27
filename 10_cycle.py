c18ad62aa8ed24836cd0dbf029e2e79c91231355
Dmitry Ulasau <dmitrushok@gmail.com>
2026-07-27 22:56:28 +0300

    add matrix two

--- 10_cycle.py
+++ 10_cycle.py
@@ -40,12 +40,54 @@
 #     print()
 
 #-----------------------------------------
-for row in range(20):
-    for col in range(50):
-        if row == 9:
-            print("-", end="")
-        elif col == 24:
-            print("|", end="")
+# for row in range(20):
+#     for col in range(50):
+#         if row == 9:
+#             print("-", end="")
+#         elif col == 24:
+#             print("|", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+#------------------ GATE -----------------------
+
+# for row in range(20):
+#     for col in range(30):
+#         if row == 0:
+#             print("-", end="")
+#         elif col == 0:
+#             print("|", end="")
+#         elif col == 29:
+#             print("|", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+#------------------ RACE -----------------------
+# for row in range(20):
+#     for col in range(50):
+
+#         if col == row + 29:
+#             print("\\", end="")
+#         elif col == -row + 19:
+#             print("/", end="")
+#         if row == 9:
+#             print("-", end="")    
+#         elif col == 24:
+#             print("|", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+#------------------ MATRIX -----------------------
+size = 5
+for row in range(size):
+    for col in range(size):
+        if row + col == size - 1:
+            print(1, end=" ")
+        elif row + col > size - 1:
+            print(2, end=" ")
         else:
-            print(" ", end="")
+            print(0, end=" ")
     print()
