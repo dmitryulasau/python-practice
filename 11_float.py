f5ffe8a055a1880dd5f74c5f99db265e38bb4fb1
Dmitry Ulasau <dmitrushok@gmail.com>
2026-08-06 23:20:33 +0300

    add float

--- 11_float.py
+++ 11_float.py
@@ -1,6 +1,31 @@
 # FLOAT
-bet = int(input("Сколько ставим? "))
-ratio = float(input("Какой коэффициент? "))
-win = round(bet * ratio, 2)
+# bet = int(input("Сколько ставим? "))
+# ratio = float(input("Какой коэффициент? "))
+# win = round(bet * ratio, 2)
 
-print(f"Потенциальный выигрыш: {win}")
+# print(f"Потенциальный выигрыш: {win}")
+
+# ---------------------
+
+# age = int(input("Введите возраст: "))
+# temperature = float(input("Введите температуру: "))
+
+# money = round(age * 1.5 * temperature, 2)
+
+# print(money)
+
+# ---------------------
+
+weight = float(input("Введите вес: "))
+height = float(input("Введите рост: "))
+
+bmi = round(weight / (height ** 2), 2)
+
+if bmi < 18.5:
+    print("У вас недобор")
+elif bmi < 25:
+    print("У вас всё хорошо!")
+elif bmi < 30:
+    print("У вас избыток :/")
+else:
+    print("ОЖИРЕНИЕ!")
